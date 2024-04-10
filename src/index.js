34027cf92aff5a2027ccd027ef2917ed5367090c
Thelby <156729092+Thelby@users.noreply.github.com>
2024-04-11 00:59:14 +0200

    Moved css and javascript to their own files and link it to the html
    Create your CSS file (e.g., styles.css).
    Link the CSS file to HTML:
    Use the <link> tag inside the <head> section.
    Example: <link rel="stylesheet" href="styles.css">.
    Create your JavaScript file (e.g., index.js).
    Link the JavaScript file to HTML:
    Use the <script> tag just before the closing </body> tag.
    Example: <script src="index.js"></script>.

---
+++ src/index.js
@@ -0,0 +1,51 @@
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#current-temperature");
+  temperatureElement.innerHTML = response.data.temperature.current;
+}
+
+function search(event) {
+  event.preventDefault();
+  let cityElement = document.querySelector("#current-city");
+  let searchInputElement = document.querySelector("#search-input");
+  let city = searchInputElement.value;
+
+  let apiKey = "3f5obf4et7d8e3bc670b0c1774a856a5";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=3f5obf4et7d8e3bc670b0c1774a856a5&units=metric`;
+
+  axios.get(apiUrl).then(displayTemperature);
+  cityElement.innerHTML = city;
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = date.getDay();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let formattedDay = days[day];
+  return `${formattedDay} ${hours}:${minutes}`;
+}
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
+
+let currentDateELement = document.querySelector("#current-date");
+let currentDate = new Date();
+
+currentDateELement.innerHTML = formatDate(currentDate);
